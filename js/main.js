9b005f5be8112b4efe0ec7152947c42d63435589
TeoPrats <pratsteo.santadorotea@gmail.com>
2019-02-04 12:56:45 +0100

    a

---
+++ js/main.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+	document.getElementById("container").style.display="flex";
+
+}
+setTimeout(function(){
+	document.getElementById("container").style.display="none";
+},3000);
+}

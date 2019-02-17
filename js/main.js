bd63bdc2342f56433280c8fb837b4547a88fa105
TeoPrats <47102266+TeoPrats@users.noreply.github.com>
2019-02-17 18:44:20 +0100

    Delete main.js

--- js/main.js
+++
@@ -1,7 +0,0 @@
-window.onload = function(){
-	document.getElementById("container").style.display="flex";
-
-}
-setTimeout(function(){
-	document.getElementById("container").style.display="none";
-},3000);
